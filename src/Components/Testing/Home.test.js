fed13f8cc5b2792c9635c22f38d94d049b077a4f
Anant Rawat <anant.rawat@sunfox.in>
2022-03-22 16:42:42 +0530

    Default enzyme code added in setupTest

--- src/Components/Testing/Home.test.js
+++ src/Components/Testing/Home.test.js
@@ -1,7 +1,3 @@
-import { configure } from "enzyme";
-import Adapter from "enzyme-adapter-react-17-updated";
-configure({ adapter: new Adapter() });
-
 import Home from "./Home";
 import { shallow } from "enzyme";
 describe("Shallow Home page", () => {
