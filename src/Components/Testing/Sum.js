91c37a07c17d9bd3e784d9b4708fcc0e71ce81d5
Anant Rawat <anant.rawat@sunfox.in>
2022-03-22 15:14:45 +0530

    Sum function testing done for not to be

--- src/Components/Testing/Sum.js
+++ src/Components/Testing/Sum.js
@@ -1,4 +1,5 @@
 function sum(a, b) {
+  // if (a == 4 && b == 5) return 0;
   return a + b;
 }
 
--- src/Components/Testing/Sum.test.js
+++ src/Components/Testing/Sum.test.js
@@ -5,11 +5,18 @@
 
 const sum = require("./Sum");
 describe("Testing Sum function", () => {
+  // value should be equal therefor  " toBe " used
+
   it("First test case", () => {
     expect(sum(3, 5)).toBe(8);
   });
 
   it("Second test case", () => {
     expect(sum(5, 0)).toBe(5);
+  });
+
+  // value should not be equal therefore " not.toBe. " used
+  it("3rd test case", () => {
+    expect(sum(4, 5)).not.toBe(9);
   });
 });
