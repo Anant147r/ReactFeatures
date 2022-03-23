e106b658572790a9c32977e76576c665adcba702
Anant Rawat <anant.rawat@sunfox.in>
2022-03-23 08:03:41 +0530

    Different elements of html checking done

--- src/Components/Testing/ComponentFormTesting/Blog.test.js
+++ src/Components/Testing/ComponentFormTesting/Blog.test.js
@@ -7,4 +7,29 @@ describe("Blog Page test", () => {
     let wrapper = shallow(<Blog />);
     expect(wrapper.find(".liCls").length).toBe(3);
   });
+
+  it("at & key check", () => {
+    let wrapper = shallow(<Blog />);
+    expect(wrapper.find(".liCls").at(0).key()).toBe("angular");
+  });
+
+  it("child check", () => {
+    let wrapper = shallow(<Blog />);
+    expect(wrapper.find(".numbers").childAt(0).type()).toBe("span");
+  });
+
+  it("children length check", () => {
+    let wrapper = shallow(<Blog />);
+    expect(wrapper.find("ul").children().length).toBe(3);
+  });
+
+  it("hasClass check", () => {
+    let wrapper = shallow(<Blog />);
+    expect(wrapper.find(".my-button").hasClass("disabled")).toBe(true);
+  });
+
+  it("class exists or not", () => {
+    let wrapper = shallow(<Blog />);
+    expect(wrapper.is(".some-class")).toBe(true);
+  });
 });
