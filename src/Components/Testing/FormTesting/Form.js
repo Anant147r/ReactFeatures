f0e0f14733bcb7bf7747b5ac304c28b71986d6e5
Anant Rawat <anant.rawat@sunfox.in>
2022-03-25 12:52:03 +0530

    Unit testing for form inside Form component

---
+++ src/Components/Testing/FormTesting/Form.js
@@ -0,0 +1,67 @@
+import React, { Component } from "react";
+// import
+const COLORS = ["blue", "red", "yellow"];
+const startingState = {
+  name: "",
+  email: "",
+  color: COLORS[0],
+};
+
+class Form extends Component {
+  state = startingState;
+  reset = () => {
+    this.setState(startingState);
+  };
+  updateField = (stateKey) => (e) => {
+    this.setState({
+      [stateKey]: e.target.value,
+    });
+  };
+  submitForm = (e) => {
+    e.preventDefault();
+    return saveUser(this.state.name, this.state.email, this.state.color);
+  };
+  render() {
+    return (
+      <form onSubmit={this.submitForm}>
+        <div className="group">
+          <label>Name</label>
+          <input
+            id="name-input"
+            type="text"
+            value={this.state.name}
+            onChange={this.updateField("name")}
+          ></input>
+          <label>Email</label>
+          <input
+            id="email-input"
+            type="email"
+            value={this.state.email}
+            onChange={this.updateField("email")}
+          ></input>
+        </div>
+
+        <div className="group">
+          <label>Favourite Colour</label>
+          <select
+            id="color-input"
+            value={this.state.color}
+            onChange={this.updateField("color")}
+          >
+            {COLORS.map((c) => {
+              <option key={c} value={c}>
+                My Favourite Color is : {c}
+              </option>;
+            })}
+          </select>
+        </div>
+        <button type="submit">Sign up</button>
+        <button type="button" onClick={this.reset}>
+          Reset
+        </button>
+      </form>
+    );
+  }
+}
+
+export default Form;
---
+++ src/Components/Testing/FormTesting/Form.test.js
@@ -0,0 +1,66 @@
+import React from "react";
+import Form from "./Form";
+import { shallow } from "enzyme";
+
+const simulateChangeOnInput = (wrapper, input, value) => {
+  wrapper.find(input).simulate("change", {
+    target: { value: value },
+  });
+  return wrapper.find(input);
+};
+
+describe("Form", () => {
+  it("Has a default color of blue selected", () => {
+    const wrapper = shallow(<Form />);
+    const colorSelect = wrapper.find("select");
+    expect(colorSelect.props().value).toEqual("blue");
+  });
+
+  it.only("Let me fill out a form and then reset it", () => {
+    const wrapper = shallow(<Form />);
+
+    // 1ST EXAMPLE
+
+    // let nameInput = wrapper.find("input").first();
+    // nameInput.simulate("change", {
+    //   target: { value: "Anant" },
+    // });
+
+    // nameInput = wrapper.find("input").first(); //if we donot do this then nameInput's value will be the old one (not the updated on)
+    // expect(nameInput.props().value).toEqual("Anant");
+
+    // 2ND EXAMPLE
+
+    // let updatedInput = simulateChangeOnInput(
+    //   wrapper,
+    //   "input#name-input",
+    //   "Anant Rawat"
+    // );
+    // expect(updatedInput.props().value).toEqual("Anant Rawat");
+
+    // 3RD EXAMPLE  - FOR RESETING THE FORM
+
+    const nameInput = simulateChangeOnInput(
+      wrapper,
+      "#name-input",
+      "Anant Rawat"
+    );
+    const emailInput = simulateChangeOnInput(
+      wrapper,
+      "#email-input",
+      "anant.rawat@gmail.com"
+    );
+    const colorInput = simulateChangeOnInput(wrapper, "#name-input", "Yellow");
+
+    expect(nameInput.props().value).toEqual("Anant Rawat");
+    expect(emailInput.props().value).toEqual("anant.rawat@gmail.com");
+    expect(colorInput.props().value).toEqual("Yellow");
+
+    const resetButton = wrapper.find(`button[type="button"]`);
+    resetButton.simulate("click");
+
+    expect(wrapper.find("#name-input").props().value).toEqual("");
+    expect(wrapper.find("#email-input").props().value).toEqual("");
+    expect(wrapper.find("#color-input").props().value).toEqual("blue");
+  });
+});
